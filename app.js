509a61b319d55d58bf478837f9c15dafde192682
Felix Chow <dong_chow@yahoo.com>
2022-07-24 09:10:31 -0700

    updated readme to include gameplay options (versus cpu and two player), added buttons for 'Versus CPU' and 'Two Players', added Connect Four board for when player clicks Two Players, removed buttons for a cleaner look when the player clicks Two Players, added styling for board

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 const board = [];
 
 // Loop through all 7 columns of the board
+<<<<<<< HEAD
 for (let i = 0; i < 7; i++) {
   board.push([])
+=======
+for (let i = 0; i <= 7; i++) {
+  for (let j = 0; j <= 6; i++) {
+    
+  }
+  // board.push([])
+>>>>>>> 48873eb344538a7072262992e6e3b4c18cb96a71
 }
 
 const gameState = {
     board: board, // from above
+<<<<<<< HEAD
     players: ['red', 'yellow'] // or whatever
+=======
+    players: []
+>>>>>>> 48873eb344538a7072262992e6e3b4c18cb96a71
   }
