5a07fd1c61a3b63aa7c618aea69d877fb283baf8
Felix Chow <dong_chow@yahoo.com>
2022-07-31 14:25:47 -0700

    fixed turn-based system for PvP, renamed button IDs to something that made more sense, debugging error with appendChild

--- app.js
+++ app.js
@@ -11,70 +11,113 @@
 //   [36, 37, 38, 39, 40, 41]
 // ];
 
-// let winningBoard = [
-//   [0, 1, 2, 3],
-//   [6, 7, 8, 9],
-//   [12, 13, 14, 15],
-//   [18, 19, 20, 21],
-//   [24, 25, 26, 27],
-//   [30, 31, 32, 33],
-//   [36, 37, 38, 39],
-//   [0, 6, 12, 18],
-//   [6, 12, 18, 24],
-//   [12, 18, 24, 30],
-//   [18, 24, 30, 36],
-//   [1, 7, 13, 19],
-//   [7, 13, 19, 25],
-//   [13, 19, 25, 31],
-//   [19, 25, 31, 37],
-//   [2, 8, 14, 20],
-//   [8, 14, 20, 26],
-//   [14, 20, 26, 32],
-//   [20, 26, 32, 38],
-//   [3, 9, 15, 21],
-//   [9, 15, 21, 27],
-//   [15, 21, 27, 33],
-//   [21, 27, 33, 39],
-//   [4, 10, 16, 22],
-//   [10, 16, 22, 28],
-//   [16, 22, 28, 34],
-//   [22, 28, 34, 40],
-//   [5, 11, 17, 23],
-//   [11, 17, 23, 29],
-//   [17, 23, 29, 35],
-//   [23, 29, 35, 41],
-//   [0, 7, 14, 21],
-//   [7, 14, 21, 28],
-//   [14, 21, 28, 35],
-//   [6, 13, 20, 27],
-//   [13, 20, 27, 34],
-//   [20, 27, 34, 41],
-//   [12, 19, 26, 33],
-//   [19, 26, 33, 40],
-//   [18, 25, 32, 39],
-
-// ];
+let winningBoard = [
+  [0, 1, 2, 3],
+  [41, 40, 39, 38],
+  [7, 8, 9, 10],
+  [34, 33, 32, 31],
+  [14, 15, 16, 17],
+  [27, 26, 25, 24],
+  [21, 22, 23, 24],
+  [20, 19, 18, 17],
+  [28, 29, 30, 31],
+  [13, 12, 11, 10],
+  [35, 36, 37, 38],
+  [6, 5, 4, 3],
+  [0, 7, 14, 21],
+  [41, 34, 27, 20],
+  [1, 8, 15, 22],
+  [40, 33, 26, 19],
+  [2, 9, 16, 23],
+  [39, 32, 25, 18],
+  [3, 10, 17, 24],
+  [38, 31, 24, 17],
+  [4, 11, 18, 25],
+  [37, 30, 23, 16],
+  [5, 12, 19, 26],
+  [36, 29, 22, 15],
+  [6, 13, 20, 27],
+  [35, 28, 21, 14],
+  [0, 8, 16, 24],
+  [41, 33, 25, 17],
+  [7, 15, 23, 31],
+  [34, 26, 18, 10],
+  [14, 22, 30, 38],
+  [27, 19, 11, 3],
+  [35, 29, 23, 17],
+  [6, 12, 18, 24],
+  [28, 22, 16, 10],
+  [13, 19, 25, 31],
+  [21, 15, 9, 3],
+  [20, 26, 32, 38],
+  [36, 30, 24, 18],
+  [5, 11, 17, 23],
+  [37, 31, 25, 19],
+  [4, 10, 16, 22],
+  [2, 10, 18, 26],
+  [39, 31, 23, 15],
+  [1, 9, 17, 25],
+  [40, 32, 24, 16],
+  [9, 17, 25, 33],
+  [8, 16, 24, 32],
+  [11, 17, 23, 29],
+  [12, 18, 24, 30],
+  [1, 2, 3, 4],
+  [5, 4, 3, 2],
+  [8, 9, 10, 11],
+  [12, 11, 10, 9],
+  [15, 16, 17, 18],
+  [19, 18, 17, 16],
+  [22, 23, 24, 25],
+  [26, 25, 24, 23],
+  [29, 30, 31, 32],
+  [33, 32, 31, 30],
+  [36, 37, 38, 39],
+  [40, 39, 38, 37],
+  [7, 14, 21, 28],
+  [8, 15, 22, 29],
+  [9, 16, 23, 30],
+  [10, 17, 24, 31],
+  [11, 18, 25, 32],
+  [12, 19, 26, 33],
+  [13, 20, 27, 34],
+];
 
 let players = ["Jack", "Bob"];
+let activePlayer = players[0];
+// console.log(activePlayer);
 // let players = [];
 
 // Assign variables to IDs
 const menu = document.querySelector(".main-menu");
 const vsCPU = document.querySelector("#versus-cpu");
-const twoPlayers = document.querySelector("#two-players");
-
-// When player clicks "Two Players" option, call initializeTwoPlayerGame
-twoPlayers.addEventListener("click", initializeTwoPlayerGame);
+const PvP = document.querySelector("#PvP");
 
 const boardDisplay = document.querySelector(".board");
 const displayCurrentPlayer = document.querySelector(".current-player");
-
 //   submitButton = document.querySelector("input");
+
+function mainMenu() {
+  // When player clicks "Versus CPU" option, call initializeVersusCPUGame
+  // vsCPU.addEventListener("click", initializeVersusCPUGame);
+  
+  // When player clicks "Two Players" option, call initializeTwoPlayerGame
+  PvP.addEventListener("click", initializePvPGame);
+}
 
 let gameState = {};
 
-function initializeTwoPlayerGame() {
+// function initializeVersusCPUGame() {
+//   menu.remove();
+//   displayVersusCPUGameMode();
+//   // getPlayerNames();
+//   displayPlayerTurn();
+//   createBoard();
+// }
+
+function initializePvPGame() {
   menu.remove();
+  // displayPvPGameMode();
   // getPlayerNames();
   displayPlayerTurn();
   createBoard();
@@ -121,17 +164,34 @@ function initializeTwoPlayerGame() {
 //   players.push([]) = gameState.player;
 // }
 
+// function displayVersusCPUGameMode() {
+//   const gameMode = document.createElement("div");
+//   gameMode.classList.add("game-mode");
+//   const gameModeTitle = document.createElement("h2");
+//   gameModeTitle.innerHTML = "Versus CPU";
+//   gameMode.appendChild(gameModeTitle);
+// }
+
+// function displayPvPGameMode() {
+//   const gameMode = document.createElement("div");
+//   gameMode.classList.add("game-mode");
+//   const gameModeTitle = document.createElement("h2");
+//   gameModeTitle.innerHTML = "PvP"
+//   gameMode.appendChild(gameModeTitle);
+// }
+
 function setPlayers() {
   gameState.players = players;
   // activePlayer = players[0];
-  
-  let activePlayer = players;
+
+  activePlayer = players;
   activePlayer = players[0];
   displayPlayerTurn(activePlayer);
+  console.log("here");
 }
 
 function displayPlayerTurn(activePlayer) {
-  
+
   if (activePlayer === players[0]) {
     const currentPlayer = document.createTextNode(`${activePlayer}\'s turn`);
     displayCurrentPlayer.appendChild(currentPlayer);
@@ -142,8 +202,8 @@ function displayPlayerTurn(activePlayer) {
 
 }
 
-function createBoard() {
 
+function createBoard() {
   for (let i = 1; i < 8; i++) {
     const divColumn = document.createElement("div");
     divColumn.classList.add("column");
@@ -162,58 +222,87 @@ function createBoard() {
   // boardDisplay.appendChild(restartButton);
 }
 
+boardDisplay.addEventListener("click", onSlotClick);
+
 function onSlotClick(event) {
   const slot = event.target.closest(".disc");
   if (!slot) {
     console.log("Not a valid space. Please click on a slot.");
   } else {
     // console.log("Slot has been clicked!");
+    // console.log("dropped");
     dropChip(slot);
   }
 
 }
 
 function dropChip(slot) {
-  let activePlayer = players;
-  activePlayer = players[0];
-  console.log(activePlayer);
+  // let activePlayer = players;
+  // activePlayer = players[0];
+  // console.log(activePlayer);
   if (activePlayer === players[0]) {
-    if (!slot.classList.contains("taken")) {
+    if (slot.classList.contains("taken")) {
+      const spotTaken = document.createTextNode("That spot's taken.");
+      displayCurrentPlayer.appendChild(spotTaken);
+      // console.log("spot taken");
+    } else {
       slot.classList.add("taken");
       slot.classList.add("player-one");
-    } else {
-      const spotTaken = document.createTextNode("That spot's taken.");
-      displayCurrentPlayer.appendChild(spotTaken);
+      activePlayer = players[1];
+      // console.log("Player one's turn is over");
     }
-    activePlayer = players[1];
-  } 
-  
-  else {
-    console.log(activePlayer);
-    // console.log(displayPlayerTurn(activePlayer));
-    if (!slot.classList.contains("taken")) {
-      slot.classList.add("taken");
-      slot.classList.add("player-two");
-    } else {
-      const spotTaken = document.createTextNode("That spot's taken.");
-      displayCurrentPlayer.appendChild(spotTaken);
+  } else {
+    // console.log(activePlayer);
+    if (activePlayer === players[1]) {
+      // console.log(displayPlayerTurn(activePlayer));
+      if (slot.classList.contains("taken")) {
+        const spotTaken = document.createTextNode("That spot's taken.");
+        displayCurrentPlayer.appendChild(spotTaken);
+        // console.log("spot taken");
+      } else {
+        slot.classList.add("taken");
+        slot.classList.add("player-two");
+        // console.log("Player two's turn is over");
+      }
+      activePlayer = players[0];
+      // console.log(activePlayer);
+      // displayPlayerTurn(activePlayer);
     }
-    activePlayer = players[0];
-    // displayPlayerTurn(activePlayer);
+  }
+}
+
+function checkBoard() {
+  for (let x = 0; x < winningBoard.length; x++) {
+    const winningSlots1 = slot[winningBoard[x][0]];
+    const winningSlots2 = slot[winningBoard[x][1]];
+    const winningSlots3 = slot[winningBoard[x][2]];
+    const winningSlots4 = slot[winningBoard[x][3]];
+    
+    if (winningSlots1.classList.contains("player-one") && winningSlots2.classList.contains("player-one") &&
+    winningSlots3.classList.contains("player-one") && winningSlots4.classList.contains("player-one")) { 
+      const result = document.createTextNode(`${players[0]} wins!`);
+      displayCurrentPlayer.appendChild(result);
+    }
+    
+    if (winningSlots1.classList.contains("player-two") && winningSlots2.classList.contains("player-two") &&
+    winningSlots3.classList.contains("player-two") && winningSlots4.classList.contains("player-two")) { 
+      const result = document.createTextNode(`${players[1]} wins!`);
+      displayCurrentPlayer.appendChild(result);
+    }
 
   }
 }
 
-// function playAgain() {
-//   gameState = {};
-//   playAgainButton.style.display = "none";
-//   initializeTwoPlayerGame();
-// }
+function playAgain() {
+  gameState = {};
+  playAgainButton.style.display = "none";
+  initializeTwoPlayerGame();
+}
 
-boardDisplay.addEventListener("click", onSlotClick);
+mainMenu();
 
 // const restartButton = document.querySelector("#restart");
 
 // if (restartButton) {
-//   restartButton.addEventListener("click", playAgain);
+  // restartButton.addEventListener("click", playAgain);
 // }
